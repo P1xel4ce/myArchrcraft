2c938038aefb8442cba5ad18c17f4c6ebe48bb0a
Lucas Pontes <105771594+P1xel4ce@users.noreply.github.com>
2022-06-26 21:40:20 -0300

    Add files via upload

--- myArchcraft.sh
+++ myArchcraft.sh
@@ -48,19 +48,19 @@ fish' > installApps.txt
 sudo pacman -S --noconfirm - < installApps.txt
 
 ##Dotfiles##
-cp -r Dotfiles/i3 /home/$USER/.config/
-cp -r Dotfiles/MangoHud /home/$USER/.config/
+cp -r /home/$USER/myArchcraft/Dotfiles/i3 /home/$USER/.config/
+cp -r /home/$USER/myArchcraft/Dotfiles/MangoHud /home/$USER/.config/
 
 ##Icons##
-cp -r icons/default /home/$USER/.icons/
+cp -r /home/$USER/myArchcraft/icons/default /home/$USER/.icons/
 
 ## /etc ##
-sudo cp -r etc/default /etc/
-sudo cp -r etc/optimus-manager /etc/
-sudo cp -r etc/sddm.conf.d /etc/
-sudo cp -r etc/x11 /etc/
-sudo cp intel-undervolt.conf /etc/
+sudo cp -r /home/$USER/myArchcraft/etc/default /etc/
+sudo cp -r /home/$USER/myArchcraft/etc/optimus-manager /etc/
+sudo cp -r /home/$USER/myArchcraft/etc/sddm.conf.d /etc/
+sudo cp -r /home/$USER/myArchcraft/etc/x11 /etc/
+sudo cp /home/$USER/myArchcraft/intel-undervolt.conf /etc/
 
 ##SDDM##
-sudo cp -r usr/sddm/Backgrounds /usr/share/sddm/themes/archcraft/
-sudo cp -r usr/sddm/theme.conf /usr/share/sddm/themes/archcraft/
+sudo cp -r /home/$USER/myArchcraft/usr/sddm/Backgrounds /usr/share/sddm/themes/archcraft/
+sudo cp -r /home/$USER/myArchcraft/usr/sddm/theme.conf /usr/share/sddm/themes/archcraft/
